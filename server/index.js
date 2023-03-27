6f0f4c4429d53c96b3cc18686f2564c94398ff60
Hugo Fossé <fosse.hugo25@gmail.com>
2023-03-27 22:18:58 +0200

    Formulaire Inscription

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const salt = bcrypt.genSaltSync(25);
+const salt = bcrypt.genSaltSync(2);
 
 app.get('/api/getTableProduct', async (req, res, next) => {
     try{
@@ -26,11 +26,26 @@ app.post('/api/newRegistration', async (req, res, next) => {
     try{
         //On vérifie si il n'existe déja pas un utilisateur avec le même nom ou email
         let buyerWithSameEmail = await query(`SELECT * FROM buyer WHERE email = "${req.body.email}" OR name ="${req.body.name}"`);
-        if(buyerWithSameEmail == 0) {
+        if(!buyerWithSameEmail == 0) {
+            const bodyRequest = JSON.stringify(req.body);
+            const queryResult = JSON.stringify(buyerWithSameEmail);
+            console.log(bodyRequest + queryResult); //queryResult renvoie des crochets autour du json donc il n'arrive pas a faire la comparaison ?
+            let sameName = null;
+            let sameEmail = null;
+            if(queryResult[0].name == bodyRequest.name) {
+                sameName = true;
+                sameEmail = false;
+            }else if(queryResult[0].email == bodyRequest.email) {
+                sameName = false;
+                sameEmail = true;
+            }
+            console.log("name : " + sameName + " email : " +sameEmail);
+           res.json({"sameName" : sameName, "sameEmail" : sameEmail,});
+        }else {
             //On hash le MDP avec la clé de hashage salt
             const hash = bcrypt.hashSync(req.body.passwd, salt);
             const currentDateTime = moment().format('YYYY-MM-DD HH:mm:ss');
-            await query(`INSERT INTO buyer (name, email, created_at, passwd_hash) VALUES (${req.body.name}, ${req.body.email}, ${currentDateTime}, ${hash})`);
+            await query(`INSERT INTO buyer (name, email, created_at, passwd_hash) VALUES ("${req.body.name}", "${req.body.email}", "${currentDateTime}", "${hash}")`);
         }    
     }catch (error) {
         console.log(error);
--- web/scripts/source.js
+++ web/scripts/source.js
@@ -1,12 +1,12 @@
-import { response } from "express";
-
 const modal = document.getElementById('modal');
 const btnInscription = document.getElementById('inscription-btn');
 const span = document.getElementsByClassName('close')[0];
 const btnConfirmInscription = document.getElementById('btn-confirm-inscription');
-const inputName = document.getElementById('username');
-const inputEmail = document.getElementById('email');
-const inputPasswd = document.getElementById('password');
+const inscriptionInputName = document.getElementById('username');
+const inscriptionInputEmail = document.getElementById('email');
+const inscriptionInputPasswd = document.getElementById('password');
+
+let inscriptionErrorText = document.getElementById('inscription-error-message');
 
 const ipAdress = "192.168.0.23";
 const APIport = "3000";
@@ -25,14 +25,16 @@ function AddProductInHTML(productContainer, product) {
 btnInscription.onclick = function() {
   modal.style.display = 'block';
 };
-
+//Si on clique sur la croix
 span.onclick = function() {
   modal.style.display = 'none';
+  inscriptionErrorText.innerHTML = "";
 };
-
+//Si
 window.onclick = function(event) {
   if (event.target === modal) {
     modal.style.display = 'none';
+    inscriptionErrorText.innerHTML = "";
   }
 };
 
@@ -50,24 +52,46 @@ fetch("http://" + ipAdress + ":" + APIport + "/api/getTableProduct", {method : '
 })
 
 btnConfirmInscription.addEventListener('click', () => {
-  event.preventDefault();
+  
   //Vérifiez si l'utilisateur a bien renseigner tout les champs
-  fetch("http://" + ipAdress + ":" + APIport + "/api/newRegistration", {
-    method : 'POST',
-    headers: {
-      "Content-Type": 'application/json'
-    },
-    body: JSON.stringify({
-      "name": inputName.innerHTML,
-      "email": inputEmail.innerHTML,
-      "passwd": inputPasswd.innerHTML,
+  if (inscriptionInputName.value.trim() === "" || inscriptionInputEmail.value.trim() === "" || inscriptionInputPasswd.value.trim() === "") {
+    event.preventDefault();
+    let errorMessage = "Veuillez remplir le champ : ";
+    if(inscriptionInputName.value.trim() === "") errorMessage += "Nom d'utilisateur, ";
+    if(inscriptionInputEmail.value.trim() === "") errorMessage += "Adresse Email, ";
+    if(inscriptionInputPasswd.value.trim() === "") errorMessage += "Mot de passe, ";
+    errorMessage = errorMessage.slice(0, -2); // pour enlever la virgule et l'espace en fin de chaine
+    inscriptionErrorText.innerHTML = "<p>" + errorMessage + "</p>";
+  }else {
+    fetch("http://" + ipAdress + ":" + APIport + "/api/newRegistration", {
+      method : 'POST',
+      headers: {
+        "Content-Type": 'application/json'
+      },
+      body: JSON.stringify({
+        "name": inscriptionInputName.value.trim(),
+        "email": inscriptionInputEmail.value.trim(),
+        "passwd": inscriptionInputPasswd.value.trim(),
+      })
     })
-  .then(response => {
-    if(response.status == 200) {
-
-    }else {
-
-    }
-  })
-  })
+    .then(response => {
+      if(response.status == 200) { // Réponse du serveur
+        response.json().then(data => {
+          console.log(data);
+          if(data.sameName == true) { //Name
+            inscriptionErrorText.innerHTML = "<p> Nom d'utilisateur déjà existant veuillez en rentrez un nouveau.</p>";
+          }else if(data.sameEmail == true){ //Email
+            inscriptionErrorText.innerHTML = "<p> Adresse Email déjà existante veuillez en rentrez une nouvelle.</p>";
+          }else {
+            console.log(data);
+          }
+        })
+      }else if(response.status == 403){ //Erreur
+        //event.preventDefault();
+        response.json().then(data => {
+          inscriptionErrorText.innerHTML = "<p>Erreur: " + data.error + "</p>";   
+        });
+      }
+    })
+  }
 })
